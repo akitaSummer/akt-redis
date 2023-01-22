fd4ad5fe97e0fa5b1970fc5acf5908034e2a5974
akitaSummer <644171127@qq.com>
2023-01-22 20:25:57 +0800

    feat: ae init

--- go/ae/ae.go
+++ go/ae/ae.go
@@ -1,6 +1,9 @@
 package ae
 
 import (
+	"log"
+	"time"
+
 	"golang.org/x/sys/unix"
 )
 
@@ -21,6 +24,8 @@ const (
 type FileCallback func(loop *AeLoop, fd int, extra interface{})
 type TimeCallback func(loop *AeLoop, fd int, extra interface{})
 
+var fe2ep = [3]uint32{0, unix.EPOLLIN, unix.EPOLLOUT}
+
 type AeFileEvent struct {
 	fd    int
 	mask  FeType
@@ -29,7 +34,7 @@ type AeFileEvent struct {
 }
 
 type AeTimeEvent struct {
-	fd       int
+	id       int
 	mask     TeType
 	when     int64 //ms
 	interval int64 // 频率
@@ -44,6 +49,15 @@ type AeLoop struct {
 	fileEventFd     int
 	timeEventNextId int
 	stop            bool
+}
+
+// 返回fileEvent的key，可读为正，可写为负
+func getFeKey(fd int, mask FeType) int {
+	if mask == AE_READABLE {
+		return fd
+	} else {
+		return fd * -1
+	}
 }
 
 func AeLoopCreate() (*AeLoop, error) {
@@ -62,28 +76,172 @@ func AeLoopCreate() (*AeLoop, error) {
 
 // 主循环
 func (loop *AeLoop) AeMain() {
+	for !loop.stop {
+		tes, fes := loop.AeWait()
+		loop.AeProcess(tes, fes)
+	}
+}
 
+// 获取最近的时间
+func (loop *AeLoop) nearestTime() int64 {
+	var nearest int64 = GetMsTime() + 1000
+	p := loop.TimeEvents
+	for p != nil {
+		if p.when < nearest {
+			nearest = p.when
+		}
+		p = p.next
+	}
+	return nearest
 }
 
 // 等待事件
 func (loop *AeLoop) AeWait() (tes []*AeTimeEvent, fes []*AeFileEvent) {
+	// 获取超时时间
+	timeout := loop.nearestTime() - GetMsTime()
+	if timeout <= 0 {
+		timeout = 10 // at least wait 10ms
+	}
+	var events [128]unix.EpollEvent
+	// 获取两次timeout之间的所有fe事件
+	n, err := unix.EpollWait(loop.fileEventFd, events[:], int(timeout))
+	if err != nil {
+		log.Printf("epoll wait warnning: %v\n", err)
+	}
+	if n > 0 {
+		log.Printf("ae get %v epoll events\n", n)
+	}
+	// 查询对应事件，并存储到fes中
+	for i := 0; i < n; i++ {
+		if events[i].Events&unix.EPOLLIN != 0 {
+			fe := loop.FileEvents[getFeKey(int(events[i].Fd), AE_READABLE)]
+			if fe != nil {
+				fes = append(fes, fe)
+			}
+		}
+		if events[i].Events&unix.EPOLLOUT != 0 {
+			fe := loop.FileEvents[getFeKey(int(events[i].Fd), AE_WRITABLE)]
+			if fe != nil {
+				fes = append(fes, fe)
+			}
+		}
+	}
+	// 查询需要执行的事件
+	now := GetMsTime()
+	p := loop.TimeEvents
+	for p != nil {
+		if p.when <= now {
+			tes = append(tes, p)
+		}
+		p = p.next
+	}
 	return nil, nil
 }
 
 // 处理事件
 func (loop *AeLoop) AeProcess(tes []*AeTimeEvent, fes []*AeFileEvent) {
+	for _, te := range tes {
+		te.cb(loop, te.id, te.extra)
+		if te.mask == AE_ONCE {
+			loop.RemoveTimeEvent(te.id)
+		} else {
+			te.when = GetMsTime() + te.interval
+		}
+	}
 
+	for _, fe := range fes {
+		fe.cb(loop, fe.fd, fe.extra)
+	}
+}
+
+func (loop *AeLoop) getEpollMask(fd int) (ev uint32) {
+	if loop.FileEvents[getFeKey(fd, AE_READABLE)] != nil {
+		ev |= fe2ep[AE_READABLE]
+	}
+	if loop.FileEvents[getFeKey(fd, AE_WRITABLE)] != nil {
+		ev |= fe2ep[AE_WRITABLE]
+	}
+	return
 }
 
 func (loop *AeLoop) AddFileEvent(fd int, mask FeType, callback FileCallback, extra interface{}) {
+	// 查询当前fd是否存在可读或可写事件
+	ev := loop.getEpollMask(fd)
+	// ev != 0 && fe2ep[mask] != 0
+	if ev&fe2ep[mask] != 0 {
+		return
+	}
+	op := unix.EPOLL_CTL_ADD
+	// 没有可读或可写事件
+	if ev != 0 {
+		op = unix.EPOLL_CTL_MOD
+	}
+
+	ev |= fe2ep[mask]
+	err := unix.EpollCtl(loop.fileEventFd, op, fd, &unix.EpollEvent{Fd: int32(fd), Events: ev})
+	if err != nil {
+		log.Printf("epoll ctr err: %v\n", err)
+		return
+	}
+
+	loop.FileEvents[getFeKey(fd, mask)] = &AeFileEvent{
+		fd:    fd,
+		mask:  mask,
+		cb:    callback,
+		extra: extra,
+	}
+	log.Printf("ae add file event fd:%v, mask:%v\n", fd, mask)
 }
 
 func (loop *AeLoop) RemoveFileEvent(fd int, mask FeType) {
+	op := unix.EPOLL_CTL_DEL
+	ev := loop.getEpollMask(fd)
+	ev &= ^fe2ep[mask]
+	if ev != 0 {
+		op = unix.EPOLL_CTL_MOD
+	}
+	err := unix.EpollCtl(loop.fileEventFd, op, fd, &unix.EpollEvent{Fd: int32(fd), Events: ev})
+	if err != nil {
+		log.Printf("epoll del err: %v\n", err)
+	}
+
+	loop.FileEvents[getFeKey(fd, mask)] = nil
+	log.Printf("ae remove file event fd:%v, mask:%v\n", fd, mask)
+}
+
+func GetMsTime() int64 {
+	return time.Now().UnixNano() / 1e6
 }
 
 func (loop *AeLoop) AddTimeEvent(mask TeType, interval int64, callback TimeCallback, extra interface{}) int {
-	return 0
+	id := loop.timeEventNextId
+	loop.timeEventNextId++
+	loop.TimeEvents = &AeTimeEvent{
+		id:       id,
+		mask:     mask,
+		interval: interval,
+		when:     GetMsTime() + interval,
+		cb:       callback,
+		extra:    extra,
+		next:     loop.TimeEvents,
+	}
+	return id
 }
 
 func (loop *AeLoop) RemoveTimeEvent(id int) {
+	p := loop.TimeEvents
+	var pre *AeTimeEvent
+	for p != nil {
+		if p.id == id {
+			if pre == nil {
+				loop.TimeEvents = p.next
+			} else {
+				pre.next = p.next
+				p.next = nil
+			}
+			break
+		}
+		pre = p
+		p = p.next
+	}
 }
--- go/ae/ae_test.go
+++ go/ae/ae_test.go
@@ -1,1 +1,80 @@
 package ae
+
+import (
+	"akt-redis/net"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func WriteCallback(loop *AeLoop, fd int, extra interface{}) {
+	buf := extra.([]byte)
+	n, err := net.Write(fd, buf)
+	if err != nil {
+		fmt.Printf("write err: %v\n", err)
+		return
+	}
+	fmt.Printf("ae write %v bytes\n", n)
+	loop.RemoveFileEvent(fd, AE_WRITABLE)
+}
+
+func ReadCallback(loop *AeLoop, fd int, extra interface{}) {
+	buf := make([]byte, 10)
+	n, err := net.Read(fd, buf)
+	if err != nil {
+		fmt.Printf("read err: %v\n", err)
+		return
+	}
+	fmt.Printf("ae read %v bytes\n", n)
+	loop.AddFileEvent(fd, AE_WRITABLE, WriteCallback, buf)
+}
+
+func AcceptCallback(loop *AeLoop, fd int, extra interface{}) {
+	cfd, err := net.Accept(fd)
+	if err != nil {
+		fmt.Printf("accept err: %v\n", err)
+		return
+	}
+	loop.AddFileEvent(cfd, AE_READABLE, ReadCallback, nil)
+}
+
+func OnceCallback(loop *AeLoop, id int, extra interface{}) {
+	t := extra.(*testing.T)
+	assert.Equal(t, 1, id)
+	fmt.Printf("time event %v done\n", id)
+}
+
+func NormalCallback(loop *AeLoop, id int, extra interface{}) {
+	end := extra.(chan struct{})
+	fmt.Printf("time event %v done\n", id)
+	end <- struct{}{}
+}
+
+func TestAe(t *testing.T) {
+	loop, err := AeLoopCreate()
+	assert.Nil(t, err)
+	sfd, err := net.TcpServer(6666)
+	loop.AddFileEvent(sfd, AE_READABLE, AcceptCallback, nil)
+	go loop.AeMain()
+
+	host := [4]byte{0, 0, 0, 0}
+	cfd, err := net.Connect(host, 6666)
+	assert.Nil(t, err)
+	msg := "helloworld"
+	n, err := net.Write(cfd, []byte(msg))
+	assert.Nil(t, err)
+	assert.Equal(t, 10, n)
+	buf := make([]byte, 10)
+	n, err = net.Read(cfd, buf)
+	assert.Nil(t, err)
+	assert.Equal(t, 10, n)
+	assert.Equal(t, msg, string(buf))
+
+	loop.AddTimeEvent(AE_ONCE, 10, OnceCallback, t)
+	end := make(chan struct{}, 2)
+	loop.AddTimeEvent(AE_NORMAL, 10, NormalCallback, end)
+	<-end
+	<-end
+	loop.stop = true
+}
